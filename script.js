2275b15ffd0bc5736d73a07d978f324e81dc97bf
Daniel Reza <daniel.reza1007@gmail.com>
2020-11-11 23:14:42 -0600

    added all rows

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// Setting the current date and time
+$(document).ready(function () {
 
+// Setting the current date and time
 function currentTime() {
 var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(now);
@@ -10,74 +11,80 @@ setInterval(function(){
    currentTime();
 },1000);
 
-// appending all hours of time blocks 
-var hour = document.createElement("span");
-var description = document.createElement("textarea");
-var saveBtn = document.createElement("button");
-// var row = document.createElement("div");
-var container = document.getElementById("container");
 
-var timeblockList = [
-    {
-        hour: "9am",
-        description: "",
-        saveBtn: "&#xf0c7;"
-    },
-    {
-        hour: "10am",
-        description: "",
-        saveBtn: "&#xf0c7;"
-    },
-    {
-        hour: "11am",
-        description: "",
-        saveBtn: "&#xf0c7;"
-    },
-    {
-        hour: "12pm",
-        description: "",
-        saveBtn: "&#xf0c7;"
-    },
-    {
-        hour: "1pm",
-        description: "",
-        saveBtn: "&#xf0c7;"
-    },
-    {
-        hour: "2pm",
-        description: "",
-        saveBtn: "&#xf0c7;"
-    },
-    {
-        hour: "3pm",
-        description: "",
-        saveBtn: "&#xf0c7;"
-    },
-    {
-        hour: "4pm",
-        description: "",
-        saveBtn: "&#xf0c7;"
-    },
-    {
-        hour: "5pm",
-        description: "",
-        saveBtn: "&#xf0c7;"
-    },
-]
 
-function renderTimeblocks() {
+})
 
-    hour.setAttribute("class", "hour");
-    description.setAttribute("class", "description");
+// // appending all hours of time blocks ~
+// var hour = document.createElement("span");
+// var description = document.createElement("textarea");
+// var saveBtn = document.createElement("button");
+// // var row = document.createElement("div");
+// var container = document.getElementById("container");
 
-    for (var i = 0; i < timeblockList.length; i++) {
-        var timeblock = timeblockList[i];
+// var timeblockList = [
+//     {
+//         hour: "9am",
+//         description: "",
+//         saveBtn: "&#xf0c7;"
+//     },
+//     {
+//         hour: "10am",
+//         description: "",
+//         saveBtn: "&#xf0c7;"
+//     },
+//     {
+//         hour: "11am",
+//         description: "",
+//         saveBtn: "&#xf0c7;"
+//     },
+//     {
+//         hour: "12pm",
+//         description: "",
+//         saveBtn: "&#xf0c7;"
+//     },
+//     {
+//         hour: "1pm",
+//         description: "",
+//         saveBtn: "&#xf0c7;"
+//     },
+//     {
+//         hour: "2pm",
+//         description: "",
+//         saveBtn: "&#xf0c7;"
+//     },
+//     {
+//         hour: "3pm",
+//         description: "",
+//         saveBtn: "&#xf0c7;"
+//     },
+//     {
+//         hour: "4pm",
+//         description: "",
+//         saveBtn: "&#xf0c7;"
+//     },
+//     {
+//         hour: "5pm",
+//         description: "",
+//         saveBtn: "&#xf0c7;"
+//     },
+// ]
 
-        var div = document.createElement("div");
-        div.appendChild(hour);
-        div.innerText = timeblock;
-        container.appendChild(div);
-    }
-}
+// function renderTimeblocks() {
 
-renderTimeblocks();
+//     hour.setAttribute("class", "hour");
+//     description.setAttribute("class", "description");
+
+//     for (var i = 0; i < timeblockList.length; i++) {
+//         var timeblock = timeblockList[i];
+
+//         var div = document.createElement("div");
+//         div.appendChild(hour);
+//         div.innerText = timeblock;
+//         container.appendChild(div);
+//     }
+// }
+
+// renderTimeblocks();
+
+// 
